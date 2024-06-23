f02ea4f877c24c00d74f8d225e6b593a63606928
nguyenthibichngoc2425 <ngocntb.23da@vku.udn.vn>
2024-06-23 22:43:31 +0700

    first

---
+++ im-vd-new/main.js
@@ -0,0 +1,19 @@
+function toggleMenu() {
+    var navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('responsive');
+}
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+        removeActiveClasses();
+        panel.classList.add("active");
+    });
+});
+
+function removeActiveClasses() {
+    panels.forEach((panel) => {
+        panel.classList.remove("active");
+    })
+}
